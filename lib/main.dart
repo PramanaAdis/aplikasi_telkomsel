da952d318b0f08b5d864c0a279d7dc8f3a4d63e7
PramanaAdis <pramanasetiawan089@gmail.com>
2024-03-13 15:43:45 +0700

    menambahkan stfull

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,17 @@ class MyApp extends StatelessWidget {
     return Container();
   }
 }
+
+class name extends StatefulWidget {
+  const name({super.key});
+
+  @override
+  State<name> createState() => _nameState();
+}
+
+class _nameState extends State<name> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
