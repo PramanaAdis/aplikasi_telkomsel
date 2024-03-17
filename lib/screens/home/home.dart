1a525f754737c6e8428289cc046f7662dddcd80c
PramanaAdis <pramanasetiawan089@gmail.com>
2024-03-17 15:50:40 +0700

    membuat header pada home.dart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -6,13 +6,56 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Home',
-          style: mediumText15,
+    Widget header() {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Hi James',
+                  style: boldText15.copyWith(color: whiteColor),
+                ),
+                Spacer(),
+                Icon(
+                  Icons.qr_code_2_rounded,
+                  color: whiteColor,
+                  size: 28,
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Image.asset(
+                  'assets/icons/Email.png',
+                  width: 34,
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Image.asset(
+                  'assets/icons/info.png',
+                  width: 28,
+                )
+              ],
+            )
+          ],
         ),
+      );
+    }
+
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.topRight,
+              stops: [0.1, 0.8, 1],
+              colors: [redColor, redColor, yellowColor])),
+      child: ListView(
+        children: [header()],
       ),
-    );
+    ));
   }
 }
