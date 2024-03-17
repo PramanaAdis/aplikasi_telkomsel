f01b8c9d12a583000a77fc8c9e09d85085c1ea8e
PramanaAdis <pramanasetiawan089@gmail.com>
2024-03-17 21:44:26 +0700

    membuat header full pada page Home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,11 +1,43 @@
+import 'dart:ffi';
+
 import 'package:aplikasi_telkomsel/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Widget headeContent(String icon, String text) {
+      return Container(
+        margin: EdgeInsets.only(right: 10),
+        padding: EdgeInsets.fromLTRB(8, 5, 2, 5),
+        decoration: BoxDecoration(
+            color: darkRedColor,
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: Row(
+          children: [
+            Image.asset(
+              icon,
+              width: 10,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              text,
+              style: boldText13.copyWith(color: whiteColor),
+            ),
+            Icon(
+              Icons.chevron_right_rounded,
+              color: whiteColor,
+            ),
+          ],
+        ),
+      );
+    }
+
     Widget header() {
       return Container(
         padding: EdgeInsets.symmetric(horizontal: 16),
@@ -36,8 +68,38 @@ class Home extends StatelessWidget {
                 ),
                 Image.asset(
                   'assets/icons/info.png',
-                  width: 28,
+                  width: 25,
                 )
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 3),
+                  child: Text(
+                    '085802750265',
+                    style: boldText15.copyWith(color: whiteColor),
+                  ),
+                ),
+                SizedBox(
+                  width: 7,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Image.asset(
+                    'assets/icons/add.png',
+                    width: 14,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                headeContent('assets/icons/love.png', '19 POIN'),
+                headeContent('assets/icons/star.png', 'Daily Check In'),
               ],
             )
           ],
@@ -51,10 +113,12 @@ class Home extends StatelessWidget {
           gradient: LinearGradient(
               begin: Alignment.centerLeft,
               end: Alignment.topRight,
-              stops: [0.1, 0.8, 1],
+              stops: [0.1, 0.6, 4],
               colors: [redColor, redColor, yellowColor])),
       child: ListView(
-        children: [header()],
+        children: [
+          header(),
+        ],
       ),
     ));
   }
